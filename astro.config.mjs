38da1d2e14c2999b369ff60b17575d85db13fe09
Marcell Kiss <kissmarcell88@gmail.com>
2024-01-10 16:01:55 +0100

    feat: base storyblok example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,33 @@
 import analogjsangular from "@analogjs/astro-angular";
 import mdx from "@astrojs/mdx";
-import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), analogjsangular(), mdx()],
+  integrations: [
+    tailwind(),
+    analogjsangular(),
+    mdx(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: "storyblok/BlogPost",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "eu", // optional,  or 'eu' (default)
+      },
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
